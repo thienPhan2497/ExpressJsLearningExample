8c0c29b59c9e87d44956419239092d259aea76d4
Thien Phan <Thienphan241197@gmail.com>
2020-04-29 02:10:54 +0700

    Add page pagination & Product page

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+require('dotenv').config();
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 
 var userRoute = require('./routes/user.route');
 var authRoute = require('./routes/auth.route');
+var productRoute = require('./routes/product.route');
 
 var authMiddleware = require('./middleware/auth.middleware');
 
@@ -16,7 +19,7 @@ app.set('views', './views');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(cookieParser(random));
+app.use(cookieParser(process.env.SESSION_SECRET));
 
 app.use(express.static('public'));
 
@@ -28,6 +31,7 @@ app.get('/', (req,res) => {
 
 app.use('/users', authMiddleware.requireAuth, userRoute);
 app.use('/auth', authRoute);
+app.use('/products', productRoute);
 
 app.listen(port, ()=>{
   console.log('Server listening on port ' + port);
